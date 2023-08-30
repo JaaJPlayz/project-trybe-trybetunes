051ba1aef53dfa7763226f0e3f706309e905ca60
JaaJPlayz <hiagoaugustofcunha@gmail.com>
2023-08-30 10:53:49 -0300

    Made the 3rd task

---
+++ src/components/music-card/index.tsx
@@ -0,0 +1,16 @@
+export default function MusicCard(musicInfo: any) {
+  const { music } = musicInfo;
+  return (
+    <div>
+      <h1>{music.trackName}</h1>
+      <h1>{music.previewUrl}</h1>
+      <audio data-testid="audio-component" src={ music.previewUrl } controls>
+        <track kind="captions" />
+        O seu navegador não suporta o elemento
+        {' '}
+        <code>audio</code>
+        .
+      </audio>
+    </div>
+  );
+}
--- src/pages/album/index.tsx
+++ src/pages/album/index.tsx
@@ -1,7 +1,42 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import getMusics from '../../services/musicsAPI';
+import Loading from '../../components/loading';
+import MusicCard from '../../components/music-card';
+
 export default function Album() {
+  const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [apiResponse, setApiResponse] = useState<any>([]);
+
+  const callGetMusics = async (itemID: any) => {
+    setLoading(true);
+    try {
+      const response = await getMusics(itemID);
+      setApiResponse(response);
+    } catch (error: any) {
+      console.log(error.message);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    callGetMusics(id);
+  }, [id]);
+
   return (
     <div>
-      <h1>Welcome to the album page</h1>
+      {loading ? <Loading /> : (
+        <div>
+          <h1 data-testid="artist-name">{apiResponse[0].artistName}</h1>
+          <h1 data-testid="album-name">{apiResponse[0].collectionName}</h1>
+          {
+            apiResponse.map((music: any, index: number) => (
+              index > 0 ? <MusicCard key={ music.trackId } music={ music } /> : null
+            ))
+          }
+        </div>
+      )}
     </div>
   );
 }
